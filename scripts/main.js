1d5defad71284d2138d9d53d80a3d120237855d7
kacevnik <kacevnik@yandex.ru>
2017-05-02 12:30:37 +0300

    Подключение библиотеки fancyBox

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,17 @@
 $(document).ready(function(){
 
+    $(".header_back").fancybox({
+        maxWidth    : 800,
+        maxHeight   : 600,
+        fitToView   : false,
+        width       : '70%',
+        height      : '70%',
+        autoSize    : false,
+        closeClick  : false,
+        openEffect  : 'none',
+        closeEffect : 'none'
+    });
+
     var spans = $('.contant_item_shadow a span');
 
     $.each(spans, function(index, value){
