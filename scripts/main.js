f2c4f2321a649875a07617bb852d8954010ed259
kacevnik <kacevnik@yandex.ru>
2017-05-02 13:40:12 +0300

    Стилизация всплывающего окна

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
 
     $(".header_back").fancybox({
-        maxWidth    : 800,
-        maxHeight   : 600,
+        padding     : 0,
+        height: 'auto',
+        maxWidth    : 320,
         fitToView   : false,
         width       : '70%',
-        height      : '70%',
         autoSize    : false,
         closeClick  : false,
         openEffect  : 'none',
